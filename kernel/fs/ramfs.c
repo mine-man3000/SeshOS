11579a75d4c3a4f4b641d20ba2257471e0fb1115
mine-man3000 <gdev0580@gmail.com>
2023-09-19 23:51:30 -0700

    techflash fixing syntax inconsistencies

--- kernel/fs/ramfs.c
+++ kernel/fs/ramfs.c
@@ -1,16 +1,15 @@
 #include <fs/ramfs.h>
-#include <mystring.h>
+#include <string.h>
 #include <stddef.h>
+#include <stdio.h>
 
 struct tar_header *headers[32];
 
-unsigned int parse()
-{
+unsigned int parse() {
     unsigned int address = *((int*)(&initramfs->address));
     unsigned int i;
  
-    for (i = 0; ; i++)
-    {
+    for (i = 0; ; i++) {
         struct tar_header *header = (struct tar_header *)address;
         headers[i] = header;
 
@@ -20,33 +19,31 @@ unsigned int parse()
         unsigned int size = getsize(header->size);
         address += ((size / 512) + 1) * 512;
         
-        if (size % 512)
+        if (size % 512) {
             address += 512;
+        }
     }
     return i;
 }
 
-unsigned int getsize(const char *in)
-{
+unsigned int getsize(const char *in) {
     unsigned int size = 0;
     unsigned int j;
     unsigned int count = 1;
  
-    for (j = 11; j > 0; j--, count *= 8)
+    for (j = 11; j > 0; j--, count *= 8) {
         size += ((in[j - 1] - '0') * count);
+    }
  
     return size;
 }
 
-char *readFile(char* fileToReadName)
-{
+char *readFile(char* fileToReadName) {
     unsigned int address = *((int*)(&initramfs->address));
     struct tar_header *header = (struct tar_header *)address;
 
-    for (int i = 0; headers[i]->filename != "\0"; i++)
-    {
-        if (strcmp(fileToReadName, headers[i]->filename))
-        {
+    for (int i = 0; headers[i]->filename != "\0"; i++) {
+        if (strcmp(fileToReadName, headers[i]->filename)) {
             header = headers[i];
             break;
         }
@@ -62,10 +59,8 @@ TwoStrings nameInPath;
 TwoStrings oldFullName;
 TwoStrings oldNameInPath;
 
-void ls()
-{
-    for (int i = 1; headers[i]->filename[0] != 0; i++)
-    {   
+void ls() {
+    for (int i = 1; headers[i]->filename[0] != 0; i++) {
         for (int i = 0; i < 100; i++) fullName.a[i] =      '\0';
         for (int i = 0; i < 100; i++) fullName.b[i] =      '\0';
         for (int i = 0; i < 100; i++) nameInPath.a[i] =    '\0';
@@ -78,17 +73,14 @@ void ls()
         fullName   = strsplit(headers[i]->filename, '/');
         nameInPath = strsplit(fullName.b, '/');
 
-        if (i > 1)
-        {   
+        if (i > 1) {
             oldFullName   = strsplit(headers[i-1]->filename, '/');
             oldNameInPath = strsplit(oldFullName.b, '/');    
-            if (!strcmp(oldNameInPath.a, nameInPath.a))
-            {
+            if (strcmp(oldNameInPath.a, nameInPath.a) == 0) {
                 printf("%s\n", nameInPath.a);
             }
         }
-        else
-        {
+        else {
             printf("%s\n", nameInPath.a);
         }
     }
