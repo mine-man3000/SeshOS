a167a6ad26c167750e6c93e430ab016f92c246e7
mine-man3000 <gdev0580@gmail.com>
2023-09-19 23:49:15 -0700

    moved memset and memcpy

--- include/memory/mem.h
+++ include/memory/mem.h
@@ -8,6 +8,4 @@ extern volatile struct limine_memmap_request mmap;
 extern uint64_t physical_kernel_start;
 
 uint64_t getMemSize();
-void *memset(void *b, int c, int len);
-void *memcpy(void* destination, const void* source, size_t num);
 int numPages();
