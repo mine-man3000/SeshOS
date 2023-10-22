0bb049deee1f2efa5ea357da5902c0099b9638b2
mineman <gdev0580@gmail.com>
2023-10-21 19:32:55 -0700

    removed the redundant printing

--- kernel/init.c
+++ kernel/init.c
@@ -9,8 +9,6 @@
 #include <memory/malloc.h>
 #include <tty.h>
 #include <acpi/acpi.h>
-#include "flanterm/flanterm.h"
-#include "flanterm/backends/fb.h"
 #include <pci/pci.h>
 #include <bootloader.h>
 
@@ -36,31 +34,20 @@ void init() {
 
     comout("TETS\n");
 
-    printf("[ %sOK %s] loading GDT\n", Green, White);
-
     create_idt();
-
-    printf("[ %sOK %s] loading IDT\n", Green, White);
 
     physical_kernel_start = kernel_address_request.response->physical_base;
 	virtual_kernel_start = kernel_address_request.response->virtual_base;
 
     hhdm_offset = hhdm_request.response->offset;
-
-    printf("[ %sOK %s] buffers filled\n", Green, White);
     
     if (!initramfs) {
         printf("[ %sINFO%s ] ramdisk not present\n", Blue, White);
         panic("ERROR: no ramdisk found! how do you expect to be able to do anything without a disk?", NULL);
     }
     parse();
-    printf("[ %sOK %s] ramdisk parsed\n", Green, White);
 
-    //ACPI IS BROKEN
     ACPI_Init();
     
-    printf("[ %sOK %s] ACPI Initialized\n", Green, White);
-    
-    // EnumeratePCI(mcfg);
-    printf("[ %sOK %s] PCI Enumerated\n", Green, White);
+    EnumeratePCI(mcfg);
 }
