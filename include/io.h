e5f18317dce3bd901cb43085b14d82e4cabb0ef4
mineman <gdev0580@gmail.com>
2023-10-21 19:30:39 -0700

    added inl and outl

--- include/io.h
+++ include/io.h
@@ -3,4 +3,6 @@ void outb(uint16_t port, uint8_t value);
 uint8_t inb(uint16_t port);
 void outw (uint16_t port, uint16_t data);
 uint16_t inw (uint16_t port);
+void outl (uint16_t port, uint32_t data);
+uint32_t inl (uint32_t port);
 void io_wait();
--- kernel/io.c
+++ kernel/io.c
@@ -5,6 +5,14 @@ void outb(uint16_t port, uint8_t value) {
         : "a"(value), "Nd"(port));
 }
 
+uint8_t inb(uint16_t port) {
+    uint8_t returnVal;
+    asm volatile("inb %1, %0"
+        : "=a"(returnVal)
+        : "Nd"(port));
+    return returnVal;
+}
+
 uint16_t inw (uint16_t port) {
     uint16_t result;
     asm("in %%dx, %%ax" : "=a" (result) : "d" (port));
@@ -15,12 +23,14 @@ void outw (uint16_t port, uint16_t data) {
     asm volatile("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
 
-uint8_t inb(uint16_t port) {
-    uint8_t returnVal;
-    asm volatile("inb %1, %0"
-        : "=a"(returnVal)
-        : "Nd"(port));
-    return returnVal;
+uint32_t inl (uint32_t port) {
+    uint16_t result;
+    asm("in %%edx, %%eax" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void outl (uint16_t port, uint32_t data) {
+    asm volatile("out %%eax, %%edx" : : "a" (data), "d" (port));
 }
 
 void io_wait() {
